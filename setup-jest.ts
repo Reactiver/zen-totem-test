7ce658819c29ac8d9852c74682314760b2b4c1e3
Dima Puris <purisdima@mail.ru>
2023-06-04 14:01:50 +0400

    feat: add email and first name fields to profile page

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,2 +1,21 @@
 import 'jest-preset-angular/setup-jest';
 import '@testing-library/jest-dom';
+
+/** taiga-ui testing fixes */
+
+// @ts-ignore
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  observe() {
+    return null;
+  }
+
+  disconnect() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
--- src/app/profile/profile-page.component.ts
+++ src/app/profile/profile-page.component.ts
@@ -1,12 +1,58 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { TuiFieldErrorPipeModule, TuiInputModule } from '@taiga-ui/kit';
+import { TuiErrorModule } from '@taiga-ui/core';
+import { TuiValidationError } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+  form = new FormGroup({
+    email: new FormControl('my-email@gmail.com'),
+    firstName: new FormControl('', [
+      textRequiredValidator('Введите имя'),
+      maxLengthValidator(255, 'Имя не должно превышать 255 символов'),
+    ]),
+  });
+}
+
+export function textRequiredValidator(message: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    return value === undefined || value === null || value === ''
+      ? new TuiValidationError(message)
+      : null;
+  };
+}
+
+export function maxLengthValidator(
+  maxLength: number,
+  message: string
+): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    return value.length > maxLength ? new TuiValidationError(message) : null;
+  };
+}
